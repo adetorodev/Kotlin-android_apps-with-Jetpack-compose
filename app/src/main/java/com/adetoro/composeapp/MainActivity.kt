3e43bebd142a555647a497ffaec19878866b37f2
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-09-17 05:08:41 +0100

    checkbox

--- app/src/main/java/com/adetoro/composeapp/MainActivity.kt
+++ app/src/main/java/com/adetoro/composeapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -22,6 +23,7 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButtonDefaults.elevation
@@ -63,194 +65,83 @@ class MainActivity : ComponentActivity(), NetworkStateListener by NetworkStateHa
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Counter()
+            CheckboxExample()
+//            AppPreview()
+//            var text by remember{ mutableStateOf("") }
+//            statelessTextField(text= text, onTextChanged =  {newText -> text = newText} )
         }
         oberserveNetworkState(this, this)
     }
 }
 
-
-//fun myTextField(icon: Icon, label: String, placeholder: String, textV: Text, modifier:Modifier = Modifier.padding(start = 5.dp).align(alignment = Alignment.CenterHorizontally)){
-//    TextField(value = textV,
-//        modifier = modifier,
-//        leadingIcon = icon,
-//        onValueChange = {newText -> text = newText},
-//        label = label,
-//        placeholder = placeholder
-//    )
-//}
-
-
 @Composable
-fun registerForm(){
-    var name = remember {
-        mutableStateOf("Name")
+fun CheckboxExample(){
+    var checked by remember {
+        mutableStateOf(false)
     }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Text(text="Register",
-            textAlign = TextAlign.Start,
-            fontSize = 34.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(all = 4.dp),
-        )
 
-           OutlinedTextField(value = "",
-               leadingIcon = {Icon(imageVector = Icons.Default.Person, contentDescription="Person Icon")},
-               onValueChange = {},
-               label = {Text(text="Your Name", fontSize = 18.sp)},
-               placeholder = {Text(text="Enter your name")}
-           )
-        OutlinedTextField(value = "",
-               leadingIcon = {Icon(imageVector = Icons.Default.Person, contentDescription="Person Icon")},
-               onValueChange = {},
-               label = {Text(text="Username", fontSize = 18.sp)},
-               placeholder = {Text(text="Enter your username")},
-           )
-        OutlinedTextField(value = "",
-               leadingIcon = {Icon(imageVector = Icons.Default.Email, contentDescription="Email Icon")},
-               onValueChange = {},
-               label = {Text(text="Email", fontSize = 18.sp)},
-               placeholder = {Text(text="Enter your email")}
-           )
-        OutlinedTextField(value = "",
-               leadingIcon = {Icon(imageVector = Icons.Default.Lock, contentDescription="Lock Icon")},
-               onValueChange = {},
-               label = {Text(text="Password", fontSize = 18.sp)},
-               placeholder = {Text(text="Enter your password")}
-           )
-        OutlinedTextField(value = "",
-               leadingIcon = {Icon(imageVector = Icons.Default.Lock, contentDescription="Lock Icon", modifier = Modifier.padding(all = 3.dp))},
-               onValueChange = {},
-               label = {Text(text="Corfirm Password", fontSize = 18.sp)},
-               placeholder = {Text(text="Enter your password")}
-           )
-        FilledTonalButton(onClick = { /*TODO*/ } ,) {
-            Text(text = "Sign Up with Apple")
-        }
-        Text(text="Continue with: ",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(all = 4.dp),
-        )
+    Checkbox(checked = checked, onCheckedChange = {
+        isChecked -> checked = isChecked
+        Toast.makeText(this, "is check $checked", Toast.LENGTH_SHORT ).show()
+    } )
 
-        Row(modifier = Modifier.padding(bottom = 14.dp)) {
-            FilledTonalButton(onClick = { /*TODO*/ }, modifier = Modifier.padding(all=4.dp)) {
-                Text(text = "Sign Up Google")
-            }
-            FilledTonalButton(onClick = { /*TODO*/ }) {
-                Text(text = "Sign Up with Apple")
-            }
-        }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun AppPreview() {
-    registerForm()
 }
 
 //@Composable
-//fun textFiled(){
-//    var text by remember {
-//        mutableStateOf(TextFieldValue())
+//fun StatefulTextField() {
+//    var name by remember {
+//        mutableStateOf("")
 //    }
-//    TextField(value = text, onValueChange = {
-//        newValue -> text = newValue
-//    } )
-//}
 //
-//@Preview(showBackground = true)
+//    var occupation by remember {
+//        mutableStateOf("")
+//    }
+//
+//    Column {
+//        OutlinedTextField(value = name, onValueChange = {
+//            newText -> name = newText
+//        }, label = {Text(text="Enter you Name")} )
+//        TextField(value = occupation, onValueChange = { newText ->
+//            occupation = newText
+//        }, label = { Text(text = "Enter Enter your Occupation") })
+//        Text(text = "Welcome  $name")
+//        Text(text="You are a $occupation")
+//    }
+//}
+
+//State Hoisting
+
+//fun StatefulTextField() {
+//    var text by remember {
+//        mutableStateOf("")
+//    }
+//
+//
+//    Column {
+//
+//        TextField(value = text, onValueChange = { newText ->
+//            text = newText
+//        }, label = { Text(text = "Enter Enter your Occupation") })
+//        Text(text = "Welcome  $text")
+//    }
+//}
+
+//@Composable
+//fun statelessTextField(
+//    text: String,
+//    onTextChanged: (String) -> Unit
+//){
+//    Column {
+//        TextField( value = text, onValueChange = onTextChanged, label ={Text(text= "Enter a text")} )
+//        Text(text="You type $text")
+//    }
+//
+//}
+
+
+
+//@Preview
 //@Composable
 //fun AppPreview() {
-//    textFiled()
+//    StatefulTextField()
 //}
-//// fill Button - summit or save
-//@Composable
-//fun fillButton(onClick: () -> Unit){
-//    Button(onClick = {
-//onClick()
-//    }) {
-//        Text(text = "Click Me")
-//    }
-//}
-//
-//// 2 Filled Tonal - add to cart, sign in
-//
-//@Composable
-//fun filledTonalButton(onClick: () -> Unit){
-//    FilledTonalButton(onClick = { onClick () }) {
-//        Text(text = "Fill Button")
-//    }
-//}
-//
-//// 3 Elevated Button
-//
-//@Composable
-//fun elevatedButton(onClick: () -> Unit){
-//    ElevatedButton(onClick = { onClick () }, elevation = ButtonDefaults.buttonElevation(defaultElevation = 8.dp))
-//
-//    {
-//        Text(text = "elevated Button")
-//    }
-//}
-//
-//// 4. outline button
-//@Composable
-//fun outlineButton(onClick: () -> Unit){
-//    OutlinedButton(onClick = { onClick () }) {
-//        Text(text = "Fill Button")
-//    }
-//}
-//
-//// Text Button
-//@Composable
-//fun textButton(onClick: () -> Unit){
-//    TextButton(onClick = { onClick () }) {
-//        Text(text = "Fill Button")
-//    }
-//}
-
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Column(modifier = Modifier
-//        .padding(all = 12.dp, )
-//        .border(1.dp, androidx.compose.ui.graphics.Color.Red)
-//        .padding(all = 16.dp)
-//        .border(2.dp, androidx.compose.ui.graphics.Color.Blue)
-//
-//    ) {
-//
-//        Text(
-//            text = "Hello Coding Master",
-//            color = androidx.compose.ui.graphics.Color.Red,
-//            fontFamily = FontFamily.Serif,
-//            fontSize = 38.sp,
-//            textDecoration = TextDecoration.combine(
-//                listOf(
-//                    TextDecoration.LineThrough,
-//                    TextDecoration.Underline
-//                )
-//            ),
-//            textAlign = TextAlign.Center,
-//            modifier = Modifier.padding(start = 10.dp)
-//        )
-//        Text(
-//            text = "Download Now"
-//        )
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//        Greeting("Android")
-//}
-
