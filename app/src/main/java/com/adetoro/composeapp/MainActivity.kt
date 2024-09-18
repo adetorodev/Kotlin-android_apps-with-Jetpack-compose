cdc59380d4324c47d9a1210623b98fc1dada356d
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-09-18 05:43:35 +0100

    LazyColumn - Recycle view

--- app/src/main/java/com/adetoro/composeapp/MainActivity.kt
+++ app/src/main/java/com/adetoro/composeapp/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -39,7 +41,7 @@ class MainActivity : ComponentActivity(), NetworkStateListener by NetworkStateHa
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun MyScaffold() {
     Scaffold(
@@ -48,22 +50,32 @@ fun MyScaffold() {
         floatingActionButton = { MyFab() }
     ) {
         paddingValues -> Log.d("Tagg", "The padding vale $paddingValues")
-        AppContent()
+        myLazyColumn()
+    }
+}
+
+@Composable
+fun myLazyColumn() {
+    val itemList = listOf<String>("Item 1 occupeer", "Item 2 the iuerig", "Item 3uirtgjnh", "Item 4 list me", "Item 5 list you good")
+    LazyColumn {
+        items(itemList){
+            item -> Text(text= item)
+        }
     }
 }
 
 //@Preview(showBackground = true)
-@Composable
-fun AppContent() {
-
-    Row (
-        modifier = Modifier.horizontalScroll(rememberScrollState())
-    ) {
-        Image(painter = painterResource(id = R.drawable.pic1), contentDescription = "Image", contentScale = ContentScale.FillBounds)
-        Image(painter = painterResource(id = R.drawable.pic2), contentDescription = "Image", contentScale = ContentScale.FillBounds)
-        Image(painter = painterResource(id = R.drawable.pic3), contentDescription = "Image", contentScale = ContentScale.FillBounds)
-    }
-}
+//@Composable
+//fun AppContent() {
+//
+//    Row (
+//        modifier = Modifier.horizontalScroll(rememberScrollState())
+//    ) {
+//        Image(painter = painterResource(id = R.drawable.pic1), contentDescription = "Image", contentScale = ContentScale.FillBounds)
+//        Image(painter = painterResource(id = R.drawable.pic2), contentDescription = "Image", contentScale = ContentScale.FillBounds)
+//        Image(painter = painterResource(id = R.drawable.pic3), contentDescription = "Image", contentScale = ContentScale.FillBounds)
+//    }
+//}
 
 //    Column(
 //        modifier = Modifier.verticalScroll(rememberScrollState())
