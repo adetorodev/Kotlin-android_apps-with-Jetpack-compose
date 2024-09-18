e3a50d7e0529f487d2afab1278d23e99f6f1bbe2
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-09-18 06:25:12 +0100

    Card

--- app/src/main/java/com/adetoro/composeapp/MainActivity.kt
+++ app/src/main/java/com/adetoro/composeapp/MainActivity.kt
@@ -17,12 +17,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -47,8 +50,19 @@ class MainActivity : ComponentActivity(), NetworkStateListener by NetworkStateHa
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            myLazyColumn()
+            Mycard()
         }
+    }
+}
+
+@Composable
+fun Mycard() {
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(8.dp).background(Blue),
+        elevation = CardDefaults.cardElevation(8.dp),
+        colors = CardDefaults.cardColors(Green)
+    ) {
+        Text(text= "Simple Card", modifier = Modifier.padding(8.dp))
     }
 }
 
