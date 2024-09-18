458f6581348dae080768b470438c953c11dd9362
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-09-18 06:37:14 +0100

    grocery-app-init

--- app/src/main/java/com/adetoro/composeapp/NetworkStateListener.kt
+++
@@ -1,9 +0,0 @@
-package com.adetoro.composeapp
-
-import android.content.Context
-import androidx.lifecycle.LifecycleOwner
-
-interface NetworkStateListener {
-    fun oberserveNetworkState(context: Context,
-                              lifecyleOwner: LifecycleOwner)
-}
