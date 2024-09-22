f7efa869c70129e463a60d0ae71afa257b41ccee
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-09-22 23:29:51 +0100

    bottom navigation - synchro

--- app/src/main/java/com/adetoro/composeapp/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/adetoro/composeapp/navigation/BottomNavigationBar.kt
@@ -11,19 +11,35 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
 fun BottomNavigationBar(navController: NavHostController) {
     val navItems = listOf(NavItem.Home, NavItem.Profile, NavItem.Settings)
-    var selectedItem by rememberSaveable {
-        mutableStateOf(0)
+//    var selectedItem by rememberSaveable {
+//        mutableStateOf(0)
+//    }
+
+    // Synchronization between NavigationBar and current screen
+
+    val navBackStackEntery by navController.currentBackStackEntryAsState()
+
+    // Current Route
+    val currentRoute = navBackStackEntery?.destination?.route
+
+    var selectedItem = navItems.indexOfFirst { it.path == currentRoute}
+
+    var selectedNavItem by rememberSaveable {
+        mutableStateOf(if (selectedItem >0 ) selectedItem else 0)
     }
+
+
     NavigationBar {
         navItems.forEachIndexed { index, item ->
             NavigationBarItem(
                 selected = selectedItem == index,
                 onClick = {
-                    selectedItem = index
+                    selectedNavItem = index
 
                     // handling the navigation to Profile Screen
 //                    Passing arguments bet5ween the screens
