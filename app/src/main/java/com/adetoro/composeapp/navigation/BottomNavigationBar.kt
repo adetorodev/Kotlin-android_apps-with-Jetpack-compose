c51542c25dd882d37b43c92a6d4e4cb26623c08b
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-09-22 16:21:10 +0100

    bottom navigation

--- app/src/main/java/com/adetoro/composeapp/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/adetoro/composeapp/navigation/BottomNavigationBar.kt
@@ -1,21 +1,46 @@
 package com.adetoro.composeapp.navigation
 
+import android.graphics.drawable.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
 
 @Composable
 fun BottomNavigationBar(navController: NavHostController) {
     val navItems = listOf(NavItem.Home, NavItem.Profile, NavItem.Settings)
-    val selectedItem by rememberSaveable {
+    var selectedItem by rememberSaveable {
         mutableStateOf(0)
     }
     NavigationBar {
         navItems.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    navController.navigate(item.path){
+                        navController.graph.startDestinationRoute?.let {
+                                route -> popUpTo(route){
+                            saveState = true
+                        }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
 
+                    }
+                },
+                icon = {
+                    Icon(imageVector = item.icon,
+                        contentDescription = item.title)
+                       },
+                label = {Text(text=item.title)}
+            )
         }
 
     }
