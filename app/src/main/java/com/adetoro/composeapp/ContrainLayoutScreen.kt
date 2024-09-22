4e4f7ef2380c2e21f2b37e1e06b5cc7d3838e908
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-09-23 00:11:02 +0100

    guideline

--- app/src/main/java/com/adetoro/composeapp/ContrainLayoutScreen.kt
+++ app/src/main/java/com/adetoro/composeapp/ContrainLayoutScreen.kt
@@ -19,25 +19,25 @@ fun ContainLayoutScreen() {
     ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
 
         val (box1, box2, text) = createRefs()
-        val guideline = createGuidelineFromTop(0.5f)
+        val guideline = createGuidelineFromStart(0.25f)
 
         Box(modifier = Modifier.size(100.dp)
             .background(Red)
             .constrainAs(box1) {
                 top.linkTo(parent.top, margin = 16.dp)
-                start.linkTo(parent.start, margin = 16.dp)
+                start.linkTo(guideline)
             }
         )
         Box(modifier = Modifier.size(100.dp)
             .background(Green)
             .constrainAs(box2){
                 top.linkTo(box1.bottom, margin= 20.dp)
-                start.linkTo(parent.start, margin = 30.dp)
+                start.linkTo(guideline)
             }
         )
         Text(text= "hello everyone there", modifier = Modifier.constrainAs(text){
             top.linkTo(box2.bottom, margin = 16.dp)
-            end.linkTo(parent.end, margin = 16.dp)
+            start.linkTo(guideline)
         })
     }
 }
